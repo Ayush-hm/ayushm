6ae1a92d3d02f6bf835d6be71ebb5fdffb15f1e7
Ayush <h.ayushm@gmail.com>
2025-01-13 22:25:33 +0530

    Changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         domains: ['github.com', 'cdn.example.com', 'bf.kendal.org', 'images.unsplash.com'],
     },
-    basePath: "/my-app",
+    basePath: "",
     output: "export",  
     reactStrictMode: true,
 };
